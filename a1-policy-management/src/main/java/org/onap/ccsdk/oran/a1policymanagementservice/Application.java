8d410e2966ffd17458ca868080c5c0ac4e1c01d2
RehanRaza <muhammad.rehan.raza@est.tech>
2020-07-28 17:44:58 +0200

    Read configuration and start-up A1 policy management service
    This is the first commit for A1 policy management service.
    The placeholder code will be added in upcoming commits.
    
    Change-Id: I2e2d4d8e73d38f9167d19220139e639df97d48d9
    Issue-ID: CCSDK-2617
    Signed-off-by: RehanRaza <muhammad.rehan.raza@est.tech>

---
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/Application.java
@@ -0,0 +1,54 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2020 Nordix Foundation. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.ccsdk.oran.a1policymanagementservice;
+
+import org.onap.ccsdk.oran.a1policymanagementservice.tasks.RefreshConfigTask;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class Application {
+
+    @Autowired
+    private RefreshConfigTask configRefresh;
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class);
+    }
+
+    /**
+     * Starts the configuration refresh task and reads the configuration.
+     *
+     * @param ctx the application context.
+     *
+     * @return the command line runner for the configuration refresh task.
+     */
+    @Bean
+    public CommandLineRunner configRefreshRunner(ApplicationContext ctx) {
+        return args -> configRefresh.start();
+    }
+
+    //TODO: add CommandLineRunner for DmaapMessageConsumer
+}
---
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/clients/A1Client.java
@@ -0,0 +1,39 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2020 Nordix Foundation. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.ccsdk.oran.a1policymanagementservice.clients;
+
+/**
+ * Common interface for 'A1' Policy access. Implementations of this interface
+ * adapts to the different southbound REST APIs supported.
+ */
+public interface A1Client {
+
+    public enum A1ProtocolType {
+        UNKNOWN, //
+        STD_V1_1, // STD A1 version 1.1
+        OSC_V1, // OSC 'A1'
+        SDNC_OSC_STD_V1_1, // SDNC_OSC with STD A1 version 1.1 southbound
+        SDNC_OSC_OSC_V1, // SDNC_OSC with OSC 'A1' southbound
+        SDNC_ONAP
+    }
+
+    // TODO: add GET, PUT, DELETE methods for policy/policytypes
+}
