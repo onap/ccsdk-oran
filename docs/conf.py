f865497df3b5ed8e2fd5aeca9dae771f88f0b642
elinuxhenrik <henrik.b.andersson@est.tech>
2020-10-01 10:52:59 +0200

    Use redoc for api docs
    Change-Id: Ia530424154226698aad6c1498c90eadf23efdea0
    Issue-ID: CCSDK-2833
    Signed-off-by: elinuxhenrik <henrik.b.andersson@est.tech>

--- docs/conf.py
+++ docs/conf.py
@@ -7,7 +7,19 @@ linkcheck_ignore = [
     'http://localhost',
 ]
 
-extensions = ['sphinx_tabs.tabs']
+extensions = ['sphinx_tabs.tabs', 'sphinxcontrib.redoc',
+]
+
+redoc = [
+            {
+                'name': 'PMS API',
+                'page': './offeredapis/pms-api',
+                'spec': './offeredapis/api_policyManagementService/swagger.json',
+                'embed': True,
+            }
+        ]
+
+redoc_uri = 'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js'
 
 intersphinx_mapping = {}
 
