87c257cff94d11b56b6afb3498f31b2baa201312
PatrikBuhr <patrik.buhr@est.tech>
2021-01-20 12:21:52 +0100

    PMS API Documentation updates
    Change-Id: I2a02145c48fd4b022b2e17b157760bd7ddb8128f
    Issue-ID: CCSDK-3077
    Signed-off-by: PatrikBuhr <patrik.buhr@est.tech>

--- a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/controllers/v2/Consts.java
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/controllers/v2/Consts.java
@@ -29,7 +29,7 @@ public class Consts {
     public static final String MANAGED_ELEMENT_ID_PARAM = "managed_element_id";
 
     public static final String V2_API_ROOT = "a1-policy/v2";
-    public static final String V2_API_NAME = "A1 Policy Management Version 2.0 (in progress)";
+    public static final String V2_API_NAME = "A1 Policy Management Version 2.0";
     public static final String V2_CONFIG_API_NAME = "Component Configuration";
 
     private Consts() {}
