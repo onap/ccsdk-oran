04057fb8feed411bb81c9b60a7e179add2739151
ecaiyanlinux <martin.c.yan@est.tech>
2021-02-24 20:11:18 +0100

    Add csit in project repo
    using scripts in the project repo for csit
    
    Signed-off-by: ecaiyanlinux <martin.c.yan@est.tech>
    Issue-ID: CCSDK-3140
    Change-Id: I08c0bbb55d6cd425c246362d40187738d26f2e43

---
+++ csit/plans/healthcheck/test/health_check.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+###############################################################################
+# Copyright 2017 Huawei Technologies Co., Ltd.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Modifications copyright (c) 2021 Nordix Foundation
+#
+###############################################################################
+
+unset http_proxy https_proxy
+SHELL_FOLDER=$(cd "$(dirname "$0")";pwd)
+docker stop $(docker ps -aq)
+docker system prune -f
+
+cd ${SHELL_FOLDER}/../config
+cp application_configuration.json.nosdnc application_configuration.json
+
+cd ${SHELL_FOLDER}/../
+
+# start NONRTRIC containers with docker compose and configuration from docker-compose.yml
+curl -L https://github.com/docker/compose/releases/download/1.27.0/docker-compose-`uname -s`-`uname -m` > docker-compose
+chmod +x docker-compose
+./docker-compose up -d
