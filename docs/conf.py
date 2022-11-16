8fae02214e862ce201b21bb44716abd4063f6bea
halil.cakal <halil.cakal@est.tech>
2022-11-16 13:24:44 +0000

    Fix breaking API documentation generation
    Add extension lfdocs-conf and replace ROBOT_VENV with ROBOT3_VENV
    
    Issue-ID: CCSDK-3815
    Change-Id: I565ee15bd5862cca0365d153358941b27befb94a
    Signed-off-by: halil.cakal <halil.cakal@est.tech>
    (cherry picked from commit 3607907428bb3c8cd569b7d3cb05480d645893d9)

--- docs/conf.py
+++ docs/conf.py
@@ -51,6 +51,23 @@ redoc = [
 
 redoc_uri = 'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js'
 
+redoc = [
+            {
+                'name': 'PMS API',
+                'page': 'offeredapis/pms-api',
+                'spec': './offeredapis/swagger/pms-api.json',
+                'embed': True,
+            },
+            {
+                'name': 'A1 ADAPTER API',
+                'page': 'offeredapis/a1-adapter-api',
+                'spec': './offeredapis/swagger/a1-adapter-api.json',
+                'embed': True,
+            }
+        ]
+
+redoc_uri = 'https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js'
+
 intersphinx_mapping = {}
 doc_url = 'https://docs.onap.org/projects'
 master_doc = 'index'
