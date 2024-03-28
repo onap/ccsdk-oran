5a1994b523c85133de3ad67939fc9fac98af1ecf
raviteja.karumuri <raviteja.karumuri@est.tech>
2024-03-28 18:22:42 +0000

    Implementing Create Policy for V3
    Issue-ID: CCSDK-4003
    Change-Id: I0da2525c8ef273f23519e9bb4332f64e92e23cb8
    Signed-off-by: Raviteja Karumuri <raviteja.karumuri@est.tech>

---
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/service/v3/AuthorizationService.java
@@ -0,0 +1,42 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.ccsdk.oran.a1policymanagementservice.service.v3;
+
+
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.authorization.AuthorizationCheck;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.authorization.PolicyAuthorizationRequest.Input.AccessType;
+import org.onap.ccsdk.oran.a1policymanagementservice.repository.Policy;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Service
+public class AuthorizationService {
+
+    @Autowired
+    private AuthorizationCheck authorization;
+
+    public Mono<Policy> authCheck (ServerWebExchange serverWebExchange, Policy policy, AccessType accessType){
+        return authorization.doAccessControl(serverWebExchange.getRequest().getHeaders().toSingleValueMap(),
+                policy, AccessType.WRITE);
+    }
+}
