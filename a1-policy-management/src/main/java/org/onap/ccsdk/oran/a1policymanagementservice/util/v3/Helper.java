963d34383d2a146a46c25172350cbbf84a212af5
raviteja.karumuri <raviteja.karumuri@est.tech>
2024-04-23 15:56:47 +0100

    Implementing remaining end points for V3 controller
    Issue-ID: CCSDK-4006
    Change-Id: Ie186c5fc6b3d5eb90297d3a36ff6160ee4667e44
    Signed-off-by: Raviteja Karumuri <raviteja.karumuri@est.tech>

--- a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/util/v3/Helper.java
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/util/v3/Helper.java
@@ -23,7 +23,9 @@ package org.onap.ccsdk.oran.a1policymanagementservice.util.v3;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import org.onap.ccsdk.oran.a1policymanagementservice.exceptions.ServiceException;
+import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.PolicyInformation;
 import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.PolicyObjectInformation;
+import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.PolicyTypeInformation;
 import org.onap.ccsdk.oran.a1policymanagementservice.repository.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -31,11 +33,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 import java.lang.invoke.MethodHandles;
 import java.time.Instant;
+import java.util.Collection;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 @Component
 public class Helper {
@@ -115,5 +120,17 @@ public class Helper {
                     ("Policy already created with ID: " + policy.getId(), HttpStatus.CONFLICT));
         }
             return Mono.just(policy);
+    }
+
+    public Flux<PolicyInformation> toFluxPolicyInformation(Collection<Policy> policyCollection) {
+        return Flux.fromIterable(policyCollection.stream()
+                .map(singlePolicy -> new PolicyInformation(singlePolicy.getId(), singlePolicy.getRic().getConfig().getRicId()))
+                .collect(Collectors.toList()));
+    }
+
+    public Collection<PolicyTypeInformation> toPolicyTypeInfoCollection(Collection<PolicyType> policyTypeCollection, Ric ric) {
+        return policyTypeCollection.stream()
+                .map(type -> new PolicyTypeInformation(type.getId(), ric.getConfig().getRicId()))
+                .collect(Collectors.toList());
     }
 }
--- a1-policy-management/src/test/java/org/onap/ccsdk/oran/a1policymanagementservice/service/v3/PolicyServiceTest.java
+++ a1-policy-management/src/test/java/org/onap/ccsdk/oran/a1policymanagementservice/service/v3/PolicyServiceTest.java
@@ -20,41 +20,45 @@
 
 package org.onap.ccsdk.oran.a1policymanagementservice.service.v3;
 
-import org.junit.jupiter.api.MethodOrderer;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.api.*;
 import org.mockito.Mockito;
 import org.onap.ccsdk.oran.a1policymanagementservice.clients.A1ClientFactory;
 import org.onap.ccsdk.oran.a1policymanagementservice.exceptions.ServiceException;
 import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.PolicyObjectInformation;
 import org.onap.ccsdk.oran.a1policymanagementservice.repository.Policies;
 import org.onap.ccsdk.oran.a1policymanagementservice.repository.Policy;
-import org.onap.ccsdk.oran.a1policymanagementservice.repository.PolicyTypes;
-import org.onap.ccsdk.oran.a1policymanagementservice.repository.Rics;
+import org.onap.ccsdk.oran.a1policymanagementservice.service.v3.AuthorizationService;
+import org.onap.ccsdk.oran.a1policymanagementservice.service.v3.PolicyService;
 import org.onap.ccsdk.oran.a1policymanagementservice.util.v3.Helper;
 import org.onap.ccsdk.oran.a1policymanagementservice.utils.v3.TestHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.util.FileSystemUtils;
 import org.springframework.web.server.ServerWebExchange;
 import org.springframework.web.server.adapter.DefaultServerWebExchange;
 import reactor.core.publisher.Mono;
+
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.nio.file.Path;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 @TestMethodOrder(MethodOrderer.MethodName.class)
 @SpringBootTest()
+@TestPropertySource(properties = { //
+        "app.vardata-directory=/tmp/pmstestv3", //
+})
 public class PolicyServiceTest {
 
-    @Autowired
-    private Rics rics;
-
-    @Autowired
-    private PolicyTypes policyTypes;
-
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
     @Autowired
     private Policies policies;
 
@@ -76,22 +80,38 @@ public class PolicyServiceTest {
     @MockBean
     private AuthorizationService authorizationService;
 
+    @AfterEach
+    public void clear() {
+        policies.clear();
+    }
+
+    @AfterAll
+    static void clearTestDir() {
+        try {
+            FileSystemUtils.deleteRecursively(Path.of("/tmp/pmstestv3"));
+        } catch (Exception e) {
+            logger.warn("Could test directory : {}", e.getMessage());
+        }
+    }
+
     @Test
-    public void testPolicyAlreadyCreatedFail() {
+    public void testPolicyAlreadyCreatedTrue() throws Exception{
 
         String policyTypeName = "uri_type_123";
         String nonRtRicId = "Ric_347";
         testHelper.addPolicyType(policyTypeName, nonRtRicId);
         ServerWebExchange serverWebExchange = Mockito.mock(DefaultServerWebExchange.class);
+        Policy policy = testHelper.buidTestPolicy(testHelper.policyObjectInfo(nonRtRicId, policyTypeName), "122344-5674");
         when(helper.jsonSchemaValidation(any())).thenReturn(Boolean.TRUE);
+        when(helper.buildPolicy(any(),any(), any(), any())).thenReturn(policy);
         when(helper.isPolicyAlreadyCreated(any(), any())).thenReturn(Mono.error(new ServiceException
-                ("Policy already created", HttpStatus.CONFLICT)));
+                ("Same policy content already created with policy ID: 122344-5674", HttpStatus.BAD_REQUEST)));
         Mono<ResponseEntity<PolicyObjectInformation>> responseMono = policyService.createPolicyService(testHelper.policyObjectInfo(nonRtRicId, policyTypeName), serverWebExchange);
-        testHelper.verifyMockError(responseMono, "Policy already created");
+        testHelper.verifyMockError(responseMono, "Same policy content already created with policy ID: 122344-5674");
     }
 
     @Test
-    public void testPolicyNotAuthorizedFail() {
+    public void testPolicyNotAuthorizedFail() throws IOException {
 
         String policyTypeName = "uri_type_123";
         String nonRtRicId = "Ric_347";
