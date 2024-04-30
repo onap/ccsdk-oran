e786ccf9a0fa0c13bfac440be687222ded46b558
raviteja.karumuri <raviteja.karumuri@est.tech>
2024-04-30 22:15:58 +0100

    Implementing other V3 Controllers
    Issue-ID: CCSDK-4008
    Change-Id: I00408d0189f26142f50c8ee3ae90fc98647e7a0d
    Signed-off-by: Raviteja Karumuri <raviteja.karumuri@est.tech>

---
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/controllers/v3/StatusControllerV3.java
@@ -0,0 +1,64 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.ccsdk.oran.a1policymanagementservice.controllers.v3;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.api.v3.HealthCheckApi;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.v2.RicRepositoryController;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.v2.StatusController;
+import org.onap.ccsdk.oran.a1policymanagementservice.mappers.v3.StatusControllerMapper;
+import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.StatusInfo;
+import org.onap.ccsdk.oran.a1policymanagementservice.service.v3.ErrorHandlingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@RestController("StatusControllerV3")
+@Tag( //
+        name = StatusControllerV3.API_NAME, //
+        description = StatusControllerV3.API_DESCRIPTION //
+)
+@RequestMapping("/a1policymanagement/v1")
+public class StatusControllerV3 implements HealthCheckApi {
+
+    public static final String API_NAME = "Health Check";
+    public static final String API_DESCRIPTION = "API used to get the health status and statistics of this service";
+
+    @Autowired
+    private StatusController statusController;
+
+    @Autowired
+    private StatusControllerMapper statusControllerMapper;
+
+    @Autowired
+    ErrorHandlingService errorHandlingService;
+
+    @Override
+    public Mono<ResponseEntity<StatusInfo>> getStatus(ServerWebExchange exchange) throws Exception {
+        return statusController.getStatus(exchange)
+                .map(statusInfoResponseEntity -> new ResponseEntity<>(statusControllerMapper.toStatusInfoV3
+                        (statusInfoResponseEntity.getBody()), statusInfoResponseEntity.getStatusCode()))
+                .doOnError(error -> errorHandlingService.handleError(error));
+    }
+}
---
+++ a1-policy-management/src/test/java/org/onap/ccsdk/oran/a1policymanagementservice/controllers/v3/RicRepositoryControllerTest.java
@@ -0,0 +1,129 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2024 OpenInfra Foundation Europe. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
+package org.onap.ccsdk.oran.a1policymanagementservice.controllers.v3;
+
+import com.google.gson.Gson;
+import org.junit.jupiter.api.*;
+import org.onap.ccsdk.oran.a1policymanagementservice.clients.SecurityContext;
+import org.onap.ccsdk.oran.a1policymanagementservice.config.TestConfig;
+import org.onap.ccsdk.oran.a1policymanagementservice.configuration.ApplicationConfig;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.OpenPolicyAgentSimulatorController;
+import org.onap.ccsdk.oran.a1policymanagementservice.controllers.v2.RappSimulatorController;
+import org.onap.ccsdk.oran.a1policymanagementservice.models.v3.ServiceRegistrationInfo;
+import org.onap.ccsdk.oran.a1policymanagementservice.repository.PolicyTypes;
+import org.onap.ccsdk.oran.a1policymanagementservice.repository.Service;
+import org.onap.ccsdk.oran.a1policymanagementservice.repository.Services;
+import org.onap.ccsdk.oran.a1policymanagementservice.utils.MockA1ClientFactory;
+import org.onap.ccsdk.oran.a1policymanagementservice.utils.v3.TestHelper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import reactor.core.publisher.Mono;
+
+import java.lang.invoke.MethodHandles;
+import java.time.Duration;
+import java.util.Objects;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = TestConfig.class)
+@TestPropertySource(properties = { //
+        "server.ssl.key-store=./config/keystore.jks", //
+        "app.webclient.trust-store=./config/truststore.jks", //
+        "app.webclient.trust-store-used=true", //
+        "app.vardata-directory=/tmp/pmstestv3", //a
+        "app.filepath=", //
+        "app.s3.bucket=" // If this is set, S3 will be used to store data.
+})
+public class RicRepositoryControllerTest {
+
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Autowired
+    private TestHelper testHelper;
+
+    @Autowired
+    private ApplicationConfig applicationConfig;
+
+    @Autowired
+    private Services services;
+
+    @Autowired
+    private MockA1ClientFactory a1ClientFactory;
+
+    @Autowired
+    private RappSimulatorController rAppSimulator;
+
+    @Autowired
+    private SecurityContext securityContext;
+
+    @Autowired
+    private OpenPolicyAgentSimulatorController openPolicyAgentSimulatorController;
+
+    @Autowired
+    private PolicyTypes policyTypes;
+
+    @Autowired
+    private Gson gson;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void init() {
+        testHelper.port = port;
+        this.applicationConfig.setAuthProviderUrl(testHelper.baseUrl() + OpenPolicyAgentSimulatorController.ACCESS_CONTROL_URL);
+    }
+
+    @AfterEach
+    void reset() {
+        services.clear();
+        a1ClientFactory.reset();
+        this.rAppSimulator.getTestResults().clear();
+        this.a1ClientFactory.setPolicyTypes(policyTypes); // Default same types in RIC and in this app
+        this.securityContext.setAuthTokenFilePath(null);
+        this.openPolicyAgentSimulatorController.getTestResults().reset();
+    }
+
+    @Test
+    public void testPutService() {
+        ServiceRegistrationInfo serviceRegistrationInfo = new ServiceRegistrationInfo("serviceId");
+        serviceRegistrationInfo.callbackUrl("http://callback.com/").keepAliveIntervalSeconds(10L);
+        Mono<ResponseEntity<String>> responseEntityMono = testHelper.restClientV3()
+                .putForEntity("/services", gson.toJson(serviceRegistrationInfo));
+        testHelper.testSuccessResponse(responseEntityMono, HttpStatus.CREATED, Objects::isNull);
+    }
+
+    @Test
+    public void testGetService() {
+        services.put(new Service("newServiceId", Duration.ofSeconds(10L),  "http://callback.com/"));
+        Mono<ResponseEntity<String>> responseEntityMono = testHelper.restClientV3().getForEntity("/services");
+        testHelper.testSuccessResponse(responseEntityMono, HttpStatus.OK, responseBoy -> responseBoy
+                .contains("http://callback.com/"));
+    }
+
+}
