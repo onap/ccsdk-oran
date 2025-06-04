01a3fb68c5e72cd5da3ae1905034219910c0b638
saul.gill <saul.gill@est.tech>
2025-06-04 16:49:38 +0100

    Make request/response logging paths configurable
    Added property to control excluded paths
    Reactive Filter will not log calls to those paths
    
    Issue-ID: CCSDK-4117
    Change-Id: I6bf920f03ba68c110336efd265e3c045eb2b6441
    Signed-off-by: saul.gill <saul.gill@est.tech>

--- a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/configuration/ReactiveEntryExitFilterConfig.java
+++ a1-policy-management/src/main/java/org/onap/ccsdk/oran/a1policymanagementservice/configuration/ReactiveEntryExitFilterConfig.java
@@ -1,3 +1,23 @@
+/*-
+ * ========================LICENSE_START=================================
+ * ONAP : ccsdk oran
+ * ======================================================================
+ * Copyright (C) 2025 OpenInfra Foundation Europe. All rights reserved.
+ * ======================================================================
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ========================LICENSE_END===================================
+ */
+
 package org.onap.ccsdk.oran.a1policymanagementservice.configuration;
 
 import org.onap.ccsdk.oran.a1policymanagementservice.util.v3.ReactiveEntryExitFilter;
@@ -6,12 +26,27 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.server.WebFilter;
+import org.springframework.web.util.pattern.PathPatternParser;
 
 @Configuration
 public class ReactiveEntryExitFilterConfig {
+
+    private ApplicationConfig applicationConfig;
+
+    public ReactiveEntryExitFilterConfig(ApplicationConfig applicationConfig) {
+        this.applicationConfig = applicationConfig;
+    }
+
     @Bean
     @Conditional(ReactiveEntryExitFilterCondition.class)
     public WebFilter reactiveEntryExitFilter() {
-        return new ReactiveEntryExitFilter();
+        // Check if the exclude path is set in the application configuration
+        String excludePath = this.applicationConfig.getLoggingReactiveEntryExitFilterExcludePath();
+        if (excludePath == null || excludePath.isEmpty()) {
+            return new ReactiveEntryExitFilter();
+        }
+        PathPatternParser parser = new PathPatternParser();
+        return new ReactiveEntryExitFilter()
+                .excludePathPatterns(parser.parse(excludePath));
     }
 }
